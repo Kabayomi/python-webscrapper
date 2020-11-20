74a03e05c2c1f4b8b4dd1603b47329b5f8aac17a
Abayomi Koyejo <abayomikoye@gmail.com>
2020-11-20 23:30:22 +0100

    ETL

---
+++ scrape.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Extract
+def extract(page):
+	headers = { "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"}
+	url = f"https://it.indeed.com/jobs?q=data+analyst&l=Italia&sort=date&lang=en&start={page}"
+	r = requests.get(url, headers)
+	soup = BeautifulSoup(r.content, "html.parser")
+	return soup
+
+# Transform
+def transform(soup):
+	divs = soup.find_all("div",class_= "jobsearch-SerpJobCard")
+	for element in divs:
+		title = element.find("a").text.strip()
+		print(title)
+	return 
+
+
+
+# Load  
+
+c = extract(0)
+#print(extract(0))
+print(transform(c))
